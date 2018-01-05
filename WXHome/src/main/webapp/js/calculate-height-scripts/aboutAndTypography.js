0a2d72dc4e0cbdb9579e2d573f9ab85361634278
WX丶SpongeBob <1261101364@qq.com>
2018-01-05 19:33:51 +0800

    Revert "wuxu"
    This reverts commit bf26d625c530ae51210b412827ccf97e515163d1.

--- WXHome/src/main/webapp/js/calculate-height-scripts/aboutAndTypography.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-
-	/* get height of the about content container */
-	var aboutTitleHeight = $('.contentTitle').outerHeight(true);
-	var aboutContentHeight = $('.contentContainerFW').outerHeight(true);
-	$("#aboutContentContainer").css("height", aboutContentHeight + aboutTitleHeight + 'px');
-	/* alert(aboutContentHeight); */
-
-})
--- WXHome/src/main/webapp/js/tooltipInit.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-
-	/* jQuery tooltips */
-	$('#memberOneTip').bubbletip($('#tip1_up'));
-	$('#memberTwoTip').bubbletip($('#tip2_up'));
-	$('#memberThreeTip').bubbletip($('#tip3_up'));
-	$('#memberFourTip').bubbletip($('#tip4_up'));
-	
-})
